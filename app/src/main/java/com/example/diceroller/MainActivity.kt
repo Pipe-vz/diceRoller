c1321e61941e19e007821a5724d983c6bc029ddc
Felipe Velasquez <fvz.1993@gmail.com>
2023-01-16 12:47:52 -0500

    Creating UI and assign the Button to a variable.

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.diceroller.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val rollBt = binding.bt1
+        rollBt.text = "let's Roll"
+
+        rollBt.setOnClickListener {
+
+        }
+
+
+    }
+}
