9d377d793470b4f48034fdad0d8dfe95e36f8e66
Felipe Velasquez <fvz.1993@gmail.com>
2023-01-17 14:16:59 -0500

    initialize a diceImage to reduce a amount of calls to the same variable inside function and make a image srcCompat

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        diceImage = binding.img1
         val rollBt = binding.bt1
         rollBt.text = "let's Roll"
 
@@ -26,9 +28,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6)+1
-        val diceImage = binding.img1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6)+1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
